75385702acd5e6c09459094043d2476aa83d5c59
Rush-MD <dilshanrashmika552@gmail.com>
2024-06-13 14:26:10 +0530

    Update hack.js

--- plugins/hack.js
+++ plugins/hack.js
@@ -1,11 +1,85 @@
-/*
-
-
-GIFTED-MD
-VERSION 3.0.0
-
-
-*/
-
-
-const _0x1fa6fe=_0x51fb;(function(_0x4d2c76,_0x26ba5a){const _0x4b5b98=_0x51fb,_0x1dd5e6=_0x4d2c76();while(!![]){try{const _0x15e60f=-parseInt(_0x4b5b98(0x18d))/0x1*(parseInt(_0x4b5b98(0x192))/0x2)+-parseInt(_0x4b5b98(0x1a0))/0x3+parseInt(_0x4b5b98(0x197))/0x4*(parseInt(_0x4b5b98(0x1a7))/0x5)+parseInt(_0x4b5b98(0x194))/0x6*(-parseInt(_0x4b5b98(0x195))/0x7)+parseInt(_0x4b5b98(0x19b))/0x8*(-parseInt(_0x4b5b98(0x19c))/0x9)+parseInt(_0x4b5b98(0x1a6))/0xa*(parseInt(_0x4b5b98(0x19d))/0xb)+parseInt(_0x4b5b98(0x199))/0xc;if(_0x15e60f===_0x26ba5a)break;else _0x1dd5e6['push'](_0x1dd5e6['shift']());}catch(_0x58102b){_0x1dd5e6['push'](_0x1dd5e6['shift']());}}}(_0x368a,0xc81f4));const {smd,sleep}=require(_0x1fa6fe(0x193));smd({'cmdname':'hack','type':'fun','info':_0x1fa6fe(0x18f),'filename':__filename},async _0x3a901c=>{const _0x27eed9=_0x1fa6fe,_0x210ec5=[_0x27eed9(0x196),'\x20█\x2010%',_0x27eed9(0x1a1),_0x27eed9(0x1a3),_0x27eed9(0x1a8),_0x27eed9(0x1a5),_0x27eed9(0x198),_0x27eed9(0x191),_0x27eed9(0x18e),'\x20█\x20█\x20█\x20█\x20█\x20█\x20█\x20█\x20█\x2090%',_0x27eed9(0x190),_0x27eed9(0x19e),_0x27eed9(0x1a2),_0x27eed9(0x1a9),'\x20HACKING\x20COMPLETED\x20',_0x27eed9(0x1a4),'\x20SUCCESSFULLY\x20SENT\x20DATA\x20AND\x20Connection\x20disconnected',_0x27eed9(0x19f)];let _0x558c19;for(const _0x360751 of _0x210ec5){_0x558c19=await _0x3a901c['send'](_0x558c19||_0x360751),await sleep(0x3e8),_0x558c19=await _0x3a901c[_0x27eed9(0x19a)](_0x558c19,_0x360751);}});function _0x51fb(_0x4f30b1,_0x42a588){const _0x368a4c=_0x368a();return _0x51fb=function(_0x51fbb1,_0x1c5210){_0x51fbb1=_0x51fbb1-0x18d;let _0x505527=_0x368a4c[_0x51fbb1];return _0x505527;},_0x51fb(_0x4f30b1,_0x42a588);}function _0x368a(){const _0x472e39=['21741840uHAUAa','edit','87544psrzwk','36tCtHTP','11UBFqMR','System\x20hyjacking\x20on\x20process..\x20\x5cn\x20Conecting\x20to\x20Server\x20error\x20to\x20find\x20404\x20','BACKLOGS\x20CLEARED','956151wQGsPJ','\x20█\x20█\x2020%','Device\x20successfully\x20connected...\x20\x5cn\x20Receiving\x20data...','\x20█\x20█\x20█\x2030%','\x20SENDING\x20LOG\x20DOCUMENTS...','\x20█\x20█\x20█\x20█\x20█\x2050%','2262030QKQhqV','105UfxATc','\x20█\x20█\x20█\x20█\x2040%','Data\x20hyjacked\x20from\x20device\x20100%\x20completed\x20\x5cn\x20killing\x20all\x20evidence\x20killing\x20all\x20malwares...','1125401CQeQvY','\x20█\x20█\x20█\x20█\x20█\x20█\x20█\x20█\x2080%','hacking\x20prank','\x20█\x20█\x20█\x20█\x20█\x20█\x20█\x20█\x20█\x20█\x20100%','\x20█\x20█\x20█\x20█\x20█\x20█\x20█\x2070%','2IDgkZF','../lib','6utCGgs','6564061mfprgo','Injecting\x20Malware','229960eoHmvB','\x20█\x20█\x20█\x20█\x20█\x20█\x2060%'];_0x368a=function(){return _0x472e39;};return _0x368a();}
+    pattern: "menus",
+    type: "MENU list",
+    info: "user",
+    dontAddCommandList: true,
+  },
+  async (_0x22514a) => {
+    try {
+      let _0x20ed34 = (
+        "\n*➮ᴜᴘ ᴛɪᴍᴇ :* " +
+        runtime(process.uptime()) +
+        "\n*➮ᴛᴏᴅᴀʏ ɪs :* " +
+        _0x22514a.date +
+        "\n*➮ɴᴏᴡ ᴛɪᴍᴇ :* " +
+        _0x22514a.time +
+        "\n\n➮Fᴏᴜɴᴅᴇʀ- Rush Team\n➮Oᴡɴᴇʀ - " +
+        Config.ownername +
+        "\n➮Nᴜᴍ - " +
+        owner.split(",")[0] +
+        "\n➮Mᴇᴍᴏ - " +
+        formatp(os.totalmem() - os.freemem()) +
+        "/" +
+        formatp(os.totalmem()) +
+        "\n\n *🧑‍💻 :* Rushmd\n\n" +
+        readmore +
+        "\n╭──❰ *ALL MENU* ❱\n│🏮 Lɪꜱᴛ\n│🏮 Cᴀᴛᴇɢᴏʀʏ\n│🏮 Hᴇʟᴘ\n│🏮 Aʟɪᴠᴇ\n│🏮 Uᴘᴛɪᴍᴇ\n│🏮 Wᴇᴀᴛʜᴇʀ\n│🏮 Lɪɴᴋ\n│🏮 Cᴘᴜ\n│🏮 Rᴇᴘᴏꜱɪᴛᴏʀʏ\n╰─────────────⦁"
+      ).trim();
+      return await _0x22514a.bot.sendUi(_0x22514a.from, {
+        caption: _0x20ed34,
+      });
+    } catch (_0x450fce) {
+      await _0x22514a.error(_0x450fce + "\nCommand:menus", _0x450fce);
+    }
+  }
+);
+Rush_patch.cmd(
+  {
+    pattern: "pala",
+    desc: "To check ping",
+    category: "user",
+    fromMe: true,
+    filename: __filename,
+  },
+  async (_0x5d887, _0x291296, { Void: _0x43ee74 }) => {
+    try {
+      if (!_0x291296) {
+        return await _0x5d887.send(
+          "*_how is pala_*"
+        );
+      }
+      let _0x584a9e = _0x291296.split(",");
+      var _0x5b0dfd;
+      var _0x3be11d;
+      let _0x17bd8a = false;
+      if (_0x5d887.quoted) {
+        let _0x1f29ea = _0x5d887.quoted.mtype;
+        if (_0x1f29ea == "stickerMessage" && _0x291296) {
+          _0x17bd8a = true;
+          _0x5b0dfd = _0x291296.split(" ")[0];
+          _0x3be11d = "sticker-" + _0x5d887.quoted.msg.fileSha256;
+        }
+      }
+      if (!_0x17bd8a && _0x584a9e.length > 1) {
+        _0x3be11d = _0x584a9e[0].trim().toLowerCase();
+        _0x5b0dfd = _0x584a9e[1].trim().toLowerCase();
+      } else if (!_0x17bd8a) {
+        return await _0x5d887.send(
+          "*_Uhh Dear, Give Cmd With New Name_*\n*Eg: _.setcmd New_Name, Cmd_Name_*"
+        );
+      }
+      if (_0x3be11d.length < 1) {
+        return await _0x5d887.reply(
+          "*_Uhh Please, Provide New_Cmd Name First_*"
+        );
+      }
+      if (global.setCmdAlias[_0x3be11d]) {
+        return await _0x5d887.send(
+          '*_"' +
+            (_0x17bd8a ? "Given Sticker" : _0x3be11d) +
+            '" Already set for "' +
+            global.setCmdAlias[_0x3be11d] +
+            '" Cmd, Please try another ' +
+            (_0x17bd8a ? "Sticker" : "Name") +
+            "_*"
+        );
+      }
